8835c35add9a1048fecb50bbce3614640ee9077e
user <g.braun@gmx.net>
2023-12-18 13:44:02 +0100

    Day 18 Part 2

--- src/main/java/org/example/Constants.java
+++ src/main/java/org/example/Constants.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.List;
 import java.util.Set;
 
 public class Constants {
@@ -15,6 +16,7 @@ public class Constants {
     static final char D = 'd';
     static final char N = 'n';
 
+    static final List<Character> DIRECTIONS = List.of(L,D,R,U);
     static final Set<Character> HORIZONTAL_DIRECTIONS = Set.of(L, R);
     static final Set<Character> VERTICAL_DIRECTIONS = Set.of(U, D);
 
--- src/main/java/org/example/Day18Part2.java
+++ src/main/java/org/example/Day18Part2.java
@@ -8,18 +8,20 @@ import java.nio.file.Paths;
 import java.util.*;
 
 import static org.example.Constants.*;
+import static org.example.Day18Part1.dig;
+import static org.example.Day18Part1.Dig;
 
-public class Day18Part1 {
+public class Day18Part2 {
 
     @SneakyThrows
     public static void main(String... args) {
         Path path = Paths.get("src/main/resources/day18");
         var lines = Files.readAllLines(path);
 
-        List<Dig> digs = lines.stream().map(Day18Part1::mapToDig).toList();
+        List<Day18Part1.Dig> digs = lines.stream().map(Day18Part2::mapToDig).toList();
 
         Coordinate currentStartCoordinate = new Coordinate(0, 0);
-        for (Dig hole : digs) {
+        for (Day18Part1.Dig hole : digs) {
             dig(currentStartCoordinate, hole);
             currentStartCoordinate = hole.getEnd();
         }
@@ -32,7 +34,7 @@ public class Day18Part1 {
             actual.setPole(HORIZONTAL_DIRECTIONS.contains(actual.getDirection()) && prev.getDirection() != succ.getDirection());
         }
 
-        Map<Integer, List<Dig>> rowToDigs = new HashMap<>();
+        Map<Integer, List<Day18Part1.Dig>> rowToDigs = new HashMap<>();
         for (var dig : digs) {
             for (int row = Math.min(dig.getStart().getRow(), dig.getEnd().getRow());
                  row <= Math.max(dig.getStart().getRow(), dig.getEnd().getRow());
@@ -46,7 +48,7 @@ public class Day18Part1 {
         var minRow = digs.stream().mapToInt(dig -> dig.getStart().getRow()).min().orElseThrow();
         var maxRow = digs.stream().mapToInt(dig -> dig.getStart().getRow()).max().orElseThrow();
 
-        var sum = 0;
+        var sum = 0L;
         for (int row = minRow; row <= maxRow; row++) {
             var isInside = false;
             int from = 0;
@@ -68,48 +70,13 @@ public class Day18Part1 {
         System.out.println(sum);
     }
 
-    static void dig(Coordinate currentStartCoordinate, Dig hole) {
-        switch (hole.getDirection()) {
-            case U -> {
-                hole.setStart(currentStartCoordinate);
-                hole.setEnd(new Coordinate(currentStartCoordinate.getRow() - hole.length, currentStartCoordinate.getColumn()));
-            }
-            case L -> {
-                hole.setStart(currentStartCoordinate);
-                hole.setEnd(new Coordinate(currentStartCoordinate.getRow(), currentStartCoordinate.getColumn() - hole.length));
-            }
-            case D -> {
-                hole.setStart(currentStartCoordinate);
-                hole.setEnd(new Coordinate(currentStartCoordinate.getRow() + hole.length, currentStartCoordinate.getColumn()));
-            }
-            case R -> {
-                hole.setStart(currentStartCoordinate);
-                hole.setEnd(new Coordinate(currentStartCoordinate.getRow(), currentStartCoordinate.getColumn() + hole.length));
-            }
-            default -> throw new RuntimeException();
-        }
-    }
 
     private static Dig mapToDig(String line) {
         String[] split = line.split(" ");
-        char direction = split[0].toLowerCase().charAt(0);
-        int length = Integer.parseInt(split[1]);
+        char direction = DIRECTIONS.get(Integer.parseInt(split[2].substring(7, 8)));
+        int length = Integer.parseInt(split[2].substring(2,7), 16);
 
         return Dig.builder().direction(direction).length(length).build();
     }
 
-    @Builder
-    @EqualsAndHashCode
-    @ToString
-    @Getter
-    static class Dig {
-        @Setter
-        private Coordinate start;
-        @Setter
-        private Coordinate end;
-        private char direction;
-        private int length;
-        @Setter
-        private boolean isPole;
-    }
 }
